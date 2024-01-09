824da3a4075387c7f10756a7dcc800021a1ae938
MauraRayo <rayomaura@gmail.com>
2024-01-09 19:41:30 +0100

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+ <BasicLayout>
+  <h2>Estamos en la home</h2>
+ </BasicLayout>
+</template>
+
+<script>
+import BasicLayout from "../layout/BasicLayout"
+
+export default {
+  name: 'Home',
+  components: {
+    BasicLayout,
+  }
+
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <BasicLayout/>
+    <h1>Estamos en Login</h1>
+
+    <router-link to="/register">
+        Crear Cuenta
+    </router-link>
+</template>
+
+<script>
+import BasicLayout from "../layout/BasicLayout"
+    export default {
+      name: "Login", 
+      components: {
+        BasicLayout
+      } 
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
